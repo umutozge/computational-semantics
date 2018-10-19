1129f8399ea628f783bf7a31ce9868b3d6083d0c
umut <umut.oezge@gmail.com>
2018-10-19 16:26:13 +0300

    1539955573

--- code/util/setutils.lisp
+++ code/util/setutils.lisp
@@ -42,6 +42,16 @@
   (funcall (intern (concatenate 'string "PICK-A-SUBSET-" (random-pick '("I" "R")))) set size))
 
 
+
+
+;;
+;; Cartesian product of a collection of sets
+;; Version 1
+;;
+;; This set does all in one full sweep; recurses over the collection (set-of-sets)
+;; and takes the product on the fly while recursing.
+;; Nested MAPCAR's might be a little cryptic, if you're not used to them.
+
 (defun cartesian-product (set-of-sets &optional (accu '(nil)))
   "computes the cartesian product of the sets in set-of-sets"
   (if (endp set-of-sets)
@@ -54,6 +64,32 @@
                             #'(lambda (y) (append x (list y)))
                             (car set-of-sets)))
                       accu)))))
+
+
+
+
+;;
+;; Cartesian product of a collection of sets
+;; Version 2
+;;
+;; First a "procedural" flavored binary cartestion product taker  (CARTESIAN-PRODUCT-BINARY);
+;; then REDUCE will be enough to apply it to 
+;; a set of sets.
+
+(defun  cartesian-product-binary (set1 set2)
+  "compute the cartesian product of the given sets -- the first set may be a set of tuples rather than atoms"
+  (let ((accu nil))
+	(dolist (x set1 accu)
+	  (dolist (y set2)
+		(setf accu (cons
+					 (append (if (listp x) x (list x)) (list y))
+					 accu))))))
+
+(defun cartesian-product-v2 (set-of-sets)
+  (reduce #'cartesian-product-binary set-of-sets))
+
+
+
 
 (defun generate-tuples (baseset n)
   "generates a random set of n-tuples from baseset"
