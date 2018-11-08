1a9192b4bc1f6ce050c8787385f19e76ac823cea
umut <umut.oezge@gmail.com>
2018-11-08 17:24:37 +0300

    1541687077

--- code/models.lisp
+++ code/models.lisp
@@ -12,14 +12,18 @@
                                  #'(lambda (x) (or x (funcall *generate-entity*)))
                                  (make-list 4 :initial-element nil)))
 
-(defparameter *vocabulary* '((0 john mary bill sue)
-                             (2 loves hates chases)
-                             (1 happy green book dog cat human sleeps walks)
-                             ))
+(defparameter *content-vocabulary* '((0 john mary bill sue)
+									 (1 happy green book dog cat human sleeps walks)
+									 (2 loves hates chases)
+									 ))
+
+(defparameter *function-vocabulary* '((is #'identity)
+									  (not #'(lambda (f)
+											  #'(lambda (x) (not (funcall f x)))))))
 
 (defun read-vocab-from-file (vocab-path-string)
   (with-open-file (instr (make-pathname :name vocab-path-string) :direction :input)
-    (setf *vocabulary* (read instr nil 'eof))))
+    (setf *content-vocabulary* (read instr nil 'eof))))
 
 (defun process-vocab-item (vocab-item)
   (let ((arity (car vocab-item))
@@ -33,15 +37,44 @@
         names))))
 
 (defun generate-model ()
-    (do ((vocab-list *vocabulary*  (cdr vocab-list))
+    (do ((vocab-list *content-vocabulary*  (cdr vocab-list))
          (model nil (append (process-vocab-item (car vocab-list)) model)))
       ((endp vocab-list) model)))
 
-(defun display-model (model)
-  (let* ((vocab (sort *vocabulary* #'< :key #'car))
+(defparameter *model* (generate-model))
+
+(defun display-model ()
+  (let* ((vocab (sort *content-vocabulary* #'< :key #'car))
          (arities (mapcar #'car vocab)))
-    (format t "~%The domain of entities: ~A~%~%" *entity-domain*)
+    (format t "~%The domain of entities: ~A~%" *entity-domain*)
     (dolist (arity arities)
-      (format t "~%~A-place names: ~%~%" arity)
-      (dolist (name (cdr (assoc arity *vocabulary*)))
-        (format t "~A:    ~A~%" name (cdr (assoc name model)))))))
+      (format t "~%~A-place names: ~%" arity)
+      (dolist (name (cdr (assoc arity *content-vocabulary*)))
+        (format t "~A:    ~A~%" name (let ((value (cdr (assoc name *model*))))
+									   (if (and (consp value ) (consp (car value)))
+										 (mapcar #'reverse value)
+										 value)))))))
+
+(defun construct-interpretation ()
+  (let ((if-table (make-hash-table)))     ; interpretation function table
+	(dolist (item *model*)
+	  (let ((vocab-item (car item))       ; vocabulary item
+			(interp (cdr item)))         ; set-theoretic interpretation 
+		(if (listp interp)
+		  (setf (gethash vocab-item if-table) (set-to-function interp))
+		  (setf (gethash vocab-item if-table) interp))))
+	(dolist (item *function-vocabulary* if-table)
+	  (let ((vocab-item (car item))    
+			(interp (cadr item)))      
+		(setf (gethash vocab-item if-table) (eval interp))))))
+
+(defparameter *interpretation* (construct-interpretation))
+
+(defun interpret (form)
+  (if (consp form)
+	(funcall (interpret (car form)) (interpret (cadr form)))
+	(gethash form *interpretation*)))
+
+(defun refresh-model ()
+  (setf *model* (generate-model))
+  (setf *interpretation* (construct-interpretation)))
